ca86f163bb4d470db76fbdac59c5fb18e0d28dbd
Juliana Milson <julianamilson@hotmail.com>
2024-05-02 22:59:26 -0300

    ok

--- criar_conta_corrente.py
+++ criar_conta_corrente.py
@@ -1,6 +1,13 @@
-
-def criar_conta_corrente():
-    # n da conta eh sequencial, iniciando em 1
-    #n da agencia eh FIXO 0001
+def criar_conta_corrente(numero_global_conta):
     # usuario pode ter mais de uma conta, mas toda conta deve pertencer a somente um user
-    print("hello!")
+    conta = {
+        'agencia' : '0001',
+        'n_conta' : numero_global_conta,
+        'saldo' : 0,
+        'limite' : 500,
+        'extrato' : "",
+        'numero_saques' : 0,
+        'limite_saques' : 3,
+    }
+
+    return conta
--- dentro_conta.py
+++ dentro_conta.py
@@ -4,14 +4,13 @@ from extrato import extrato as f_extrato
 from saque import saque
 
 _menu_conta = """
-Escolha uma acao
+Escolha uma ação
 
 [1] Depositar
 [2] Sacar
 [3] Extrato
 [4] Criar conta corrente
 [0] Sair
-
 => """
 saldo = 0
 limite = 500
@@ -23,24 +22,25 @@ def pegar_valor(funcao=""):
     return float(input(f"Informe o valor do {funcao}: "))
 
 def dentro_conta(usuario):
-    opcao = input(_menu_conta)
-    if opcao == "1":
-        valor = pegar_valor(funcao="depósito")
-        deposito(saldo, valor, extrato)
+    while True:
+        opcao = input(_menu_conta)
+        if opcao == "1":
+            valor = pegar_valor(funcao="depósito")
+            deposito(saldo, valor, extrato)
 
-    elif opcao == "2":
-        valor = pegar_valor(funcao="saque")
-        saque(saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES)
+        elif opcao == "2":
+            valor = pegar_valor(funcao="saque")
+            saque(saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES)
 
-    elif opcao == "3":
-        f_extrato(saldo, extrato=extrato)
+        elif opcao == "3":
+            f_extrato(saldo, extrato=extrato)
 
-    elif opcao == '4':
-        usuario["contas_correntes"].append(criar_conta_corrente())
-        print(usuario["contas_correntes"])
+        elif opcao == '4':
+            usuario[1]["contas_correntes"].append("bingus")
+            print(usuario[1]["contas_correntes"])
 
-    elif opcao == "0":
-        return
+        elif opcao == "0":
+            break
 
-    else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
