491c176f3778efae0f648f45f38bdde106f3bea3
Juliana Milson <julianamilson@hotmail.com>
2024-05-02 22:13:56 -0300

    proper prompt output

--- extrato.py
+++ extrato.py
@@ -1,5 +1,7 @@
 def extrato(saldo, extrato=""):  #positional e keyword only  
-    print("EXTRATO".center(50, '=')
-            +("Não foram realizadas movimentações." if not extrato else extrato)
-            +f"Saldo: R$ {saldo:.2f}" + "EXTRATO".center(50, '=')
-            )
+    print("EXTRATO".center(50, '='))
+    if not extrato or not saldo:
+        print("Não foram realizadas movimentações.")
+    else:
+        print(f"Saldo: R$ {saldo:.2f}")
+    print("".center(50, '='))
