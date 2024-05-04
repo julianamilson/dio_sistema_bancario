5c0cd4b5d4fa8323caf6a0be130cff6ec9298c4a
Juliana Milson <julianamilson@hotmail.com>
2024-05-04 00:25:20 -0300

    the logics may be done, but this code needs urgent refactoration

--- dentro_conta.py
+++ dentro_conta.py
@@ -13,38 +13,66 @@ Escolha uma ação
 [0] Sair
 => """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
-
 def pegar_valor(funcao=""):
     return float(input(f"Informe o valor do {funcao}: "))
+
+def pegar_contas(contas):
+    i = 0
+    for conta in contas:
+        i += 1
+    return i
 
 def dentro_conta(usuario, contas_bancarias):
     while True:
         opcao = input(_menu_conta)
-        if opcao == "1":
-            valor = pegar_valor(funcao="depósito")
-            deposito(saldo, valor, extrato)
+        tem_conta_corrente = pegar_contas(usuario[1]["contas_correntes"])
+        if tem_conta_corrente > 0:
+            indice = usuario[1]["contas_correntes"][0] - 1
+            aux_conta_update = contas_bancarias[indice][1]
 
-        elif opcao == "2":
-            valor = pegar_valor(funcao="saque")
-            saque(saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES)
-
-        elif opcao == "3":
-            f_extrato(saldo, extrato=extrato)
+        if (opcao == '1' or opcao == '2' or opcao == '3') and tem_conta_corrente == 0:
+            print("Usuário não tem conta corrente.")
 
         elif opcao == '4':
-            numero_conta = len(contas_bancarias) + 1
-            nova_conta = criar_conta_corrente(numero_conta)
-            aux = [usuario[0], nova_conta]
-            contas_bancarias.append(aux)
-            usuario[1]["contas_correntes"].append(numero_conta)
-
-        elif opcao == "0":
-            break
+                numero_conta = len(contas_bancarias) + 1
+                nova_conta = criar_conta_corrente(numero_conta)
+                aux = [usuario[0], nova_conta]
+                contas_bancarias.append(aux)
+                usuario[1]["contas_correntes"].append(numero_conta)
+                tem_conta_corrente = pegar_contas(usuario[1]["contas_correntes"]) + 1
 
         else:
-            print("Operação inválida, por favor selecione novamente a operação desejada.")
+            indice = usuario[1]["contas_correntes"][0] - 1
+            saldo = contas_bancarias[indice][1]['saldo']
+            extrato = contas_bancarias[indice][1]['extrato']
+            limite = contas_bancarias[indice][1]['limite']
+            numero_saques = contas_bancarias[indice][1]['numero_saques']
+            limite_saques = contas_bancarias[indice][1]['limite_saques']
+    
+            if opcao == "1":
+                valor = pegar_valor(funcao="depósito")
+                saldo, extrato = deposito(saldo, valor, extrato)
+
+            elif opcao == "2":
+                valor = pegar_valor(funcao="saque")
+                saldo, extrato, numero_saques= saque(saldo, valor, extrato, limite, numero_saques, limite_saques)
+
+            elif opcao == "3":
+                f_extrato(saldo, extrato=extrato)
+
+            elif opcao == "0":
+                contas_bancarias[indice][1]['saldo'] = saldo
+                contas_bancarias[indice][1]['extrato'] = extrato
+                contas_bancarias[indice][1]['limite'] = limite
+                contas_bancarias[indice][1]['numero_saques'] = numero_saques
+                contas_bancarias[indice][1]['limite_saques'] = limite_saques
+                break
+
+            else:
+                print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+            contas_bancarias[indice][1]['saldo'] = saldo
+            contas_bancarias[indice][1]['extrato'] = extrato
+            contas_bancarias[indice][1]['limite'] = limite
+            contas_bancarias[indice][1]['numero_saques'] = numero_saques
+            contas_bancarias[indice][1]['limite_saques'] = limite_saques
